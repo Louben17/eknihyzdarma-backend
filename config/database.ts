58fcf69a54e12c6a3d1838f1e3751f0b523e2f8a
Jakub Kozel <louben17@gmail.com>
2026-02-12 09:03:05 +0100

    fix: oprava SSL konfigurace pro PostgreSQL databázi

--- config/database.ts
+++ config/database.ts
@@ -7,9 +7,9 @@ export default ({ env }) => {
         client: 'postgres',
         connection: {
           connectionString: env('DATABASE_URL'),
-          ssl: env.bool('DATABASE_SSL', false) && {
+          ssl: env.bool('DATABASE_SSL', false) ? {
             rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-          },
+          } : false,
         },
         pool: {
           min: env.int('DATABASE_POOL_MIN', 2),
